a256d10d80f90216a1af5ea0d5691a76153d19a1
UC-raderee <raderee@mail.uc.edu>
2022-02-01 03:55:43 -0500

    Lab 3

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -4,6 +4,23 @@
 
 } 
 write-host(getIP) 
-$IP = getIP 
+$IP = getIP
 
-Write-Host(“This machine’s IP is $IP”) 
+function GetVersion{ 
+
+    Get-Host | Select-Object Version 
+
+} 
+write-host(GetVersion) 
+$Version = GetVersion
+
+function GetDate{ 
+
+    Get-Date 
+
+} 
+write-host(GetDate) 
+$Date = GetDate
+
+
+Send-MailMessage -To "raderee@mail.uc.edu" -From "erader27@gmail.com" -Subject "IT3038C Windows SysInfo" -Body "My IP Address is: $IP. User is $env:USERNAME. Hostname is $env:computername. Powershell version is $Version. Today is $Date.  Yes I did this at 4:00 a.m., please don't judge me." -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential) 
